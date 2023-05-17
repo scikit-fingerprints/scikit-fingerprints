502c62fd30d438d6a04359256e781f73aed2acf4
Arch4ngel21 <adammichal657@gmail.com>
2023-05-17 12:37:07 +0200

    Changed library for concurrency from multiprocessing to joblib.

--- base.py
+++ base.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
-import multiprocessing as mp
+from abc import ABC
+
+import joblib
+from joblib import Parallel, delayed
 from typing import Union
 
 import numpy as np
@@ -11,7 +13,7 @@ from featurizers.descriptors import fp_descriptors
 class FingerprintTransformer(ABC):
     def __init__(self, n_jobs):
         if n_jobs == -1:
-            self.n_jobs = mp.cpu_count()
+            self.n_jobs = joblib.cpu_count()
         else:
             self.n_jobs = n_jobs
 
@@ -19,6 +21,12 @@ class FingerprintTransformer(ABC):
         self.fp_descriptor_name: str = ""
 
     def _transform(self, X: Union[pd.DataFrame, np.ndarray]) -> np.ndarray:
+        """
+        Helper function to be executed in each sub-process.
+
+        :param X: subset of original X data
+        :return: np.array containing calculated fingerprints for each molecule
+        """
         result = np.array(
             [
                 fp_descriptors[self.fp_descriptor_name](x, **self.fp_args)
@@ -37,10 +45,11 @@ class FingerprintTransformer(ABC):
         if batch_size == 0:
             batch_size = 1
 
-        with mp.Pool(processes=self.n_jobs) as pool:
-            args = [
-                X[i : i + batch_size] for i in range(0, len(X), batch_size)
-            ]
-            results = pool.map(self._transform, args)
+        args = [X[i : i + batch_size] for i in range(0, len(X), batch_size)]
+
+        with joblib.parallel_backend("loky", n_jobs=self.n_jobs):
+            results = Parallel()(
+                delayed(self._transform)(X_sub) for X_sub in args
+            )
 
         return np.concatenate(results)
