20989073971779fd732a1f01e2b52b8d27b4c0b2
Arch4ngel21 <adammichal657@gmail.com>
2023-07-31 14:25:31 +0200

    Divided map4 branch into map4 and e3fp for each fingerprint.

--- tests/fingerprints_tests.py
+++ tests/fingerprints_tests.py
@@ -23,21 +23,9 @@ from featurizers.fingerprints import (
     TopologicalTorsionFingerprint,
     ERGFingerprint,
     MAP4Fingerprint,
-    E3FP,
 )
 from featurizers.map4 import GetMAP4Fingerprint
 from rdkit.Chem.rdReducedGraphs import GetErGFingerprint
-
-from e3fp.fingerprint.metrics.fprint_metrics import tanimoto
-from e3fp.pipeline import fprints_from_smiles
-from e3fp.conformer.generate import (
-    NUM_CONF_DEF,
-    POOL_MULTIPLIER_DEF,
-    RMSD_CUTOFF_DEF,
-    MAX_ENERGY_DIFF_DEF,
-    FORCEFIELD_DEF,
-    SEED_DEF,
-)
 
 
 @pytest.fixture
@@ -214,69 +202,6 @@ def test_map4_fingerprint(example_molecules):
     X_seq = np.array([GetMAP4Fingerprint(x, is_folded=True) for x in X_2])
 
     assert np.all(X_map4 == X_seq)
-
-
-def test_e3fp(example_molecules):
-    X = example_molecules
-
-    X_2 = X.copy()
-
-    confgen_params = {
-        "num_conf": NUM_CONF_DEF,
-        "first": 1,
-        "pool_multiplier": POOL_MULTIPLIER_DEF,
-        "rmsd_cutoff": RMSD_CUTOFF_DEF,
-        "max_energy_diff": MAX_ENERGY_DIFF_DEF,
-        "forcefield": FORCEFIELD_DEF,
-        "seed": 0,
-    }
-
-    fprint_params = {
-        "bits": 4096,
-        "radius_multiplier": 1.5,
-        "rdkit_invariants": True,
-    }
-
-    # Concurrent
-    e3fp_fp = E3FP(
-        confgen_params, fprint_params, is_folded=False, n_jobs=-1, verbose=0
-    )
-    X_e3fp = e3fp_fp.transform(X)
-
-    confgen_params = {
-        "num_conf": NUM_CONF_DEF,
-        "first": 1,
-        "pool_multiplier": POOL_MULTIPLIER_DEF,
-        "rmsd_cutoff": RMSD_CUTOFF_DEF,
-        "max_energy_diff": MAX_ENERGY_DIFF_DEF,
-        "forcefield": FORCEFIELD_DEF,
-        "seed": 0,
-    }
-
-    # Sequential
-    X_seq = np.array(
-        [
-            fprints_from_smiles(
-                x,
-                x,
-                confgen_params=confgen_params,
-                fprint_params=fprint_params,
-            )
-            for x in X_2
-        ],
-        dtype=object,
-    )
-    X_seq = X_seq.flatten()
-
-    if type(X_seq[0]) is list:
-        new_X_seq = []
-        for x_seq in X_seq:
-            for fp in x_seq:
-                new_X_seq.append(fp)
-        X_seq = np.array(new_X_seq, dtype=object)
-
-    for i in range(len(X_e3fp)):
-        assert tanimoto(X_e3fp[i], X_seq[i])
 
 
 def test_input_validation(example_molecules):
