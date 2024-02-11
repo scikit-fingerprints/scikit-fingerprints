5e1f7f49d3200d58550a170bb4abb253078c9f8a
my-alaska <piotrztych@wp.pl>
2024-02-11 21:06:56 +0100

    MHFP rdkit implementation

--- skfp/__init__.py
+++ skfp/__init__.py
@@ -5,6 +5,6 @@ from skfp.fingerprints.ecfp import ECFP
 from skfp.fingerprints.erg import ERGFingerprint
 from skfp.fingerprints.maccs_keys import MACCSKeysFingerprint
 from skfp.fingerprints.map4 import MAP4Fingerprint
-from skfp.fingerprints.mhfp import MHFP
+from skfp.fingerprints.mhfp import MHFP, MHFPlegacy
 from skfp.fingerprints.rdk import RDKitFingerprint
 from skfp.fingerprints.topological_torsion import TopologicalTorsionFingerprint
--- skfp/fingerprints/mhfp.py
+++ skfp/fingerprints/mhfp.py
@@ -10,6 +10,59 @@ from skfp.fingerprints.base import FingerprintTransformer
 class MHFP(FingerprintTransformer):
     def __init__(
         self,
+        n_permutations: int = 2048,
+        radius: int = 3,
+        rings: bool = True,
+        isomeric: bool = False,
+        kekulize: bool = True,
+        min_radius: int = 1,
+        random_state: int = 0,
+        sparse: bool = False,
+        count: bool = False,
+        n_jobs: int = None,
+        verbose: int = 0,
+    ):
+        self.n_permutations = n_permutations
+        self.radius = radius
+        self.rings = rings
+        self.isomeric = isomeric
+        self.kekulize = kekulize
+        self.min_radius = min_radius
+        super().__init__(
+            n_jobs=n_jobs,
+            sparse=sparse,
+            count=count,
+            verbose=verbose,
+            random_state=random_state,
+        )
+
+    def _calculate_fingerprint(
+        self, X: Union[pd.DataFrame, np.ndarray, list[str]]
+    ) -> Union[np.ndarray, spsparse.csr_array]:
+        X = self._validate_input(X)
+        from rdkit.Chem.rdMHFPFingerprint import MHFPEncoder
+
+        encoder = MHFPEncoder(self.n_permutations, self.random_state)
+
+        X = MHFPEncoder.EncodeMolsBulk(
+            encoder,
+            X,
+            radius=self.radius,
+            rings=self.rings,
+            isomeric=self.isomeric,
+            kekulize=self.kekulize,
+            min_radius=self.min_radius,
+        )
+
+        if self.sparse:
+            return spsparse.csr_array(X)
+        else:
+            return np.array(X)
+
+
+class MHFPlegacy(FingerprintTransformer):
+    def __init__(
+        self,
         dimensions: int = 1024,
         radius: int = 2,
         random_state: int = 0,
