9a4f6e6d4373d439752abbf099236c64e79d4e15
j-adamczyk <jakubadamczyk10@gmail.com>
2024-03-16 23:35:30 +0100

    Add pattern fingerprint

---
+++ tests/fingerprints/pattern.py
@@ -0,0 +1,18 @@
+import numpy as np
+from fingerprints import PatternFingerprint
+from rdkit.Chem.rdmolops import PatternFingerprint as RDKitPatternFingerprint
+from scipy.sparse import csr_array
+
+
+def test_pattern_bit_fingerprint(smiles_list, mols_list):
+    pattern_fp = PatternFingerprint(sparse=False, n_jobs=-1)
+    X_skfp = pattern_fp.transform(smiles_list)
+    X_rdkit = np.array([RDKitPatternFingerprint(mol) for mol in mols_list])
+    assert np.array_equal(X_skfp, X_rdkit)
+
+
+def test_pattern_sparse_bit_fingerprint(smiles_list, mols_list):
+    pattern_fp = PatternFingerprint(sparse=True, n_jobs=-1)
+    X_skfp = pattern_fp.transform(smiles_list)
+    X_rdkit = csr_array([RDKitPatternFingerprint(mol) for mol in mols_list])
+    assert np.array_equal(X_skfp.data, X_rdkit.data)
