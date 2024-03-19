09ac871b03cb08ff190fe26d1977919e115ea1a7
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2024-03-19 09:23:38 +0100

    Add SECFP fingerprint (#84)
    * Cleanup fingerprints calculation
    
    * Start refactoring tests
    
    * Refactor rest of tests
    
    * Further refactoring of tests
    
    * Fix E3FP
    
    * Fix EState tests
    
    * Remove unnecessary random_state
    
    * Further cleanup
    
    * Cleanup Github tests workflow
    
    * Add SECFP fingerprint
    
    * Update pre-commit config
    
    * Update import path
    
    * Remove pytest from main dependencies
    
    * Remove special generators code
    
    * Add option to configure number of mols in tests
    
    * Remove unused code
    
    * Add SECFP fingerprint
    
    * Update import path
    
    * Update Numpy/Scipy parsing
    
    * Update poetry.lock
    
    * Add SECFP fingerprint
    
    * Update import path
    
    * Remove unused count parameter
    
    * Fix import

---
+++ tests/fingerprints/secfp.py
@@ -0,0 +1,25 @@
+import numpy as np
+from rdkit.Chem.rdMHFPFingerprint import MHFPEncoder
+from scipy.sparse import csr_array
+
+from skfp.fingerprints import SECFPFingerprint
+
+
+def test_secfp_bit_fingerprint(smiles_list, mols_list):
+    secfp_fp = SECFPFingerprint(sparse=False, n_jobs=-1)
+    X_skfp = secfp_fp.transform(smiles_list)
+
+    encoder = MHFPEncoder(2048, 0)
+    X_rdkit = np.array([encoder.EncodeSECFPMol(x) for x in mols_list])
+
+    assert np.array_equal(X_skfp, X_rdkit)
+
+
+def test_secfp_sparse_bit_fingerprint(smiles_list, mols_list):
+    secfp_fp = SECFPFingerprint(sparse=True, n_jobs=-1)
+    X_skfp = secfp_fp.transform(smiles_list)
+
+    encoder = MHFPEncoder(2048, 0)
+    X_rdkit = csr_array([encoder.EncodeSECFPMol(x) for x in mols_list])
+
+    assert np.array_equal(X_skfp.data, X_rdkit.data)
