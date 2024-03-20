283368f6f99c30049ed3fe302d9f2950e4301228
j-adamczyk <jakubadamczyk10@gmail.com>
2024-03-20 09:59:09 +0100

    Remove "bit" from test names

--- tests/fingerprints/pattern.py
+++ tests/fingerprints/pattern.py
@@ -5,14 +5,14 @@ from scipy.sparse import csr_array
 from skfp.fingerprints import PatternFingerprint
 
 
-def test_pattern_bit_fingerprint(smiles_list, mols_list):
+def test_pattern_fingerprint(smiles_list, mols_list):
     pattern_fp = PatternFingerprint(sparse=False, n_jobs=-1)
     X_skfp = pattern_fp.transform(smiles_list)
     X_rdkit = np.array([RDKitPatternFingerprint(mol) for mol in mols_list])
     assert np.array_equal(X_skfp, X_rdkit)
 
 
-def test_pattern_sparse_bit_fingerprint(smiles_list, mols_list):
+def test_pattern_sparse_fingerprint(smiles_list, mols_list):
     pattern_fp = PatternFingerprint(sparse=True, n_jobs=-1)
     X_skfp = pattern_fp.transform(smiles_list)
     X_rdkit = csr_array([RDKitPatternFingerprint(mol) for mol in mols_list])
