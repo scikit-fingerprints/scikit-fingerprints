68af857236e59e9ca6ff67b220e12978dbf23a96
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2024-03-23 10:40:38 +0100

    PubChem fingerprint (#96)
    * Update pre-commit hooks
    
    * Add first fixes with Pyright
    
    * Mypy changes
    
    * Added mypy and fixed types
    
    * Update poetry.lock
    
    * Add atom pair counts
    
    * Finish PubChem fingerprint
    
    * Add PubChem fingerprint tests
    
    * Fix test names
    
    * Add a comment

---
+++ tests/fingerprints/pubchem.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from skfp.fingerprints import PubChemFingerprint
+
+
+def test_pubchem_bit_fingerprint(smiles_list, mols_list):
+    pubchem_fp = PubChemFingerprint(count=False, sparse=False, n_jobs=-1)
+    X_skfp = pubchem_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(mols_list), 881)
+    assert np.all(np.isin(X_skfp, [0, 1]))
+
+
+def test_pubchem_count_fingerprint(smiles_list, mols_list):
+    pubchem_fp = PubChemFingerprint(count=True, sparse=False, n_jobs=-1)
+    X_skfp = pubchem_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(mols_list), 757)
+    assert np.issubdtype(X_skfp.dtype, int) and np.all(X_skfp >= 0)
+
+
+def test_pubchem_sparse_bit_fingerprint(smiles_list, mols_list):
+    pubchem_fp = PubChemFingerprint(count=False, sparse=True, n_jobs=-1)
+    X_skfp = pubchem_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(mols_list), 881)
+    assert np.allclose(X_skfp.data, 1)
+
+
+def test_pubchem_sparse_count_fingerprint(smiles_list, mols_list):
+    pubchem_fp = PubChemFingerprint(count=True, sparse=True, n_jobs=-1)
+    X_skfp = pubchem_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(mols_list), 757)
+    assert np.issubdtype(X_skfp.dtype, int) and np.all(X_skfp.data > 0)
