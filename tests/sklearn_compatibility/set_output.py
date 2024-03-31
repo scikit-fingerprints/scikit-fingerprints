2fcb63ce1214d858e9d928b82e81d9285fd93be9
j-adamczyk <jakubadamczyk10@gmail.com>
2024-03-31 22:06:07 +0200

    Add set_output tests

--- tests/sklearn_compatibility/set_output.py
+++ tests/sklearn_compatibility/set_output.py
@@ -1,17 +1,16 @@
 import inspect
 
 import pandas as pd
+import sklearn
 
 import skfp.fingerprints
 
 
 def test_individual_set_output(mols_conformers_list):
-    # loop through all fingerprint classes
     for name, obj in inspect.getmembers(skfp.fingerprints):
         if not inspect.isclass(obj):
             continue
 
-        # check that class properly sets the output format
         fp = obj()
         fp.set_output(transform="pandas")
         output = fp.transform(mols_conformers_list)
@@ -20,3 +19,20 @@ def test_individual_set_output(mols_conformers_list):
                 f"For fingerprint {name} output was of type {type(output)} "
                 f"instead of pd.DataFrame"
             )
+
+
+def test_global_set_output(mols_conformers_list):
+    with sklearn.config_context(transform_output="pandas"):
+        # loop through all fingerprint classes
+        for name, obj in inspect.getmembers(skfp.fingerprints):
+            if not inspect.isclass(obj):
+                continue
+
+            # check that class properly sets the output format
+            fp = obj()
+            output = fp.transform(mols_conformers_list)
+            if not isinstance(output, pd.DataFrame):
+                raise AssertionError(
+                    f"For fingerprint {name} output was of type {type(output)} "
+                    f"instead of pd.DataFrame"
+                )
