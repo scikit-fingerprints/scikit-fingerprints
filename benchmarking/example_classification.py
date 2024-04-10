963d475292762fc63508181132f2b346a565dc56
mjste <mj.stefanik@gmail.com>
2024-04-10 23:27:08 +0200

    fix + refactor example_classification.py

--- benchmarking/example_classification.py
+++ benchmarking/example_classification.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 from time import time
 
@@ -10,33 +11,37 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import MinMaxScaler
 
-from skfp import (
-    AtomPairFingerprint,
-    ECFPFingerprint,
-    ERGFingerprint,
-    MACCSKeysFingerprint,
-    MAP4Fingerprint,
-    MHFPFingerprint,
-    TopologicalTorsionFingerprint,
-)
+import skfp.fingerprints
 
-dataset_names = [
-    "ogbg-molhiv",
-    "ogbg-molbace",
-    "ogbg-molbbbp",
-]
-property_names = [
-    "HIV_active",
-    "Class",
-    "p_np",
+fingerprint_classes = [
+    cls
+    for name, cls in inspect.getmembers(skfp.fingerprints, predicate=inspect.isclass)
 ]
 
-for dataset_name, property_name in zip(dataset_names, property_names):
+SIZE = 125
+
+dataset_params = [
+    ("ogbg-molhiv", "HIV_active"),
+    ("ogbg-molbace", "Class"),
+    ("ogbg-molbbbp", "p_np"),
+]
+
+
+classifier_parameters = [
+    (RandomForestClassifier, {"n_jobs": -1, "class_weight": "balanced"}),
+    (
+        LogisticRegression,
+        {"class_weight": "balanced", "penalty": None, "n_jobs": -1},
+    ),
+    (lgb.LGBMClassifier, {"n_jobs": -1, "class_weight": "balanced", "verbose": -1}),
+]
+
+for dataset_name, property_name in dataset_params:
     dataset = GraphPropPredDataset(name=dataset_name, root="../dataset")
     split_idx = dataset.get_idx_split()
-    train_idx = np.array(split_idx["train"])
-    valid_idx = np.array(split_idx["valid"])
-    test_idx = np.array(split_idx["test"])
+    train_idx = np.array(split_idx["train"])[:SIZE]
+    valid_idx = np.array(split_idx["valid"])[:SIZE]
+    test_idx = np.array(split_idx["test"])[:SIZE]
 
     dataframe = pd.read_csv(
         f"../dataset/{'_'.join(dataset_name.split('-'))}/mapping/mol.csv.gz"
@@ -56,52 +61,28 @@ for dataset_name, property_name in zip(dataset_names, property_names):
 
     records = []
 
-    fp_names = [
-        "ECFP",
-        "Atom Pairs",
-        "Topological Torsion",
-        "MACCS Keys",
-        "ERG",
-        "MAP4",
-        "MHFP",
-    ]
-    fprints = [
-        ECFPFingerprint,
-        AtomPairFingerprint,
-        TopologicalTorsionFingerprint,
-        MACCSKeysFingerprint,
-        ERGFingerprint,
-        MAP4Fingerprint,
-        MHFPFingerprint,
-    ]
-
-    clf_names = ["RF", "LogReg", "LGBM"]
-    classifiers = [
-        RandomForestClassifier,
-        LogisticRegression,
-        lgb.LGBMClassifier,
-    ]
-    classifier_kwargs = [
-        {"n_jobs": -1, "class_weight": "balanced"},
-        {
-            "class_weight": "balanced",
-            "penalty": None,
-            "n_jobs": -1,
-        },
-        {"n_jobs": -1, "class_weight": "balanced", "verbose": 0},
-    ]
-
     np.random.seed(42)
 
-    for fingerprint, fp_name in zip(fprints, fp_names):
+    for fingerprint in fingerprint_classes:
+        fp_name = fingerprint.__name__
         records.append({})
         records[-1]["fp_name"] = fp_name
-        print(fp_name, "Fingerprint")
+        print(fp_name)
         start = time()
         fp_transformer = fingerprint(n_jobs=-1)
-        X_fp_train = fp_transformer.transform(X_train)
-        X_fp_valid = fp_transformer.transform(X_valid)
-        X_fp_test = fp_transformer.transform(X_test)
+
+        try:
+            X_fp_train = fp_transformer.fit_transform(X_train)
+            X_fp_valid = fp_transformer.transform(X_valid)
+            X_fp_test = fp_transformer.transform(X_test)
+        except RuntimeError as e:
+            print(f" - Error: {e}")
+            records[-1]["error"] = str(e)
+            continue
+        except ValueError as e:
+            print(f" - Error: {e}")
+            records[-1]["error"] = str(e)
+            continue
 
         scaler = MinMaxScaler()
         X_fp_train = scaler.fit_transform(X_fp_train)
@@ -112,18 +93,25 @@ for dataset_name, property_name in zip(dataset_names, property_names):
         execution_time = end - start
         print(f" - Time of fingerprints computing : {round(execution_time,2)}s")
         records[-1]["execution_time"] = execution_time
-        for classifier, clf_name, clf_kwargs in zip(
-            classifiers, clf_names, classifier_kwargs
-        ):
+
+        for classifier, clf_kwargs in classifier_parameters:
+            clf_name = classifier.__name__
             scores = []
             scores_valid = []
-            for epoch in range(10):
-                clf = classifier(random_state=epoch, **clf_kwargs)
-                clf.fit(X_fp_train, y_train)
-                scores.append(roc_auc_score(y_test, clf.predict_proba(X_fp_test)[:, 1]))
-                scores_valid.append(
-                    roc_auc_score(y_valid, clf.predict_proba(X_fp_valid)[:, 1])
-                )
+            try:
+                for epoch in range(10):
+                    clf = classifier(random_state=epoch, **clf_kwargs)
+                    clf.fit(X_fp_train, y_train)
+                    scores.append(
+                        roc_auc_score(y_test, clf.predict_proba(X_fp_test)[:, 1])
+                    )
+                    scores_valid.append(
+                        roc_auc_score(y_valid, clf.predict_proba(X_fp_valid)[:, 1])
+                    )
+            except ValueError as e:
+                print(f" - Error: {e}")
+                records[-1][clf_name + "_error"] = str(e)
+                continue
             score = np.average(scores)
             std = np.std(scores)
             score_valid = np.average(scores_valid)
