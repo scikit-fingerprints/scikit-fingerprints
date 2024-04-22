bf2945266138dffe260b76e11d56e2563ea9ec9a
Michał Stefanik <mj.stefanik@gmail.com>
2024-04-22 21:24:06 +0200

    Fix + refactor example_classification.py (#129)
    * Changed logistic regression to CV
    
    * Impute zeros instead of NaNs
    
    * Add pre-calculated mols and conformers
    
    * Make dataset directory independent of Working Directory
    
    * always use conformers
    
    * don't scale and impute descriptors

--- benchmarking/example_classification.py
+++ benchmarking/example_classification.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 from time import time
 
@@ -5,41 +6,66 @@ import lightgbm as lgb
 import numpy as np
 import pandas as pd
 from ogb.graphproppred import GraphPropPredDataset
+from rdkit.Chem import MolFromSmiles
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
 
-from skfp import (
-    AtomPairFingerprint,
-    ECFPFingerprint,
-    ERGFingerprint,
-    MACCSKeysFingerprint,
-    MAP4Fingerprint,
-    MHFPFingerprint,
-    TopologicalTorsionFingerprint,
-)
+import skfp.fingerprints
+from skfp.preprocessing import ConformerGenerator
 
-dataset_names = [
-    "ogbg-molhiv",
-    "ogbg-molbace",
-    "ogbg-molbbbp",
-]
-property_names = [
-    "HIV_active",
-    "Class",
-    "p_np",
+fingerprint_classes = [
+    cls
+    for name, cls in inspect.getmembers(skfp.fingerprints, predicate=inspect.isclass)
 ]
 
-for dataset_name, property_name in zip(dataset_names, property_names):
-    dataset = GraphPropPredDataset(name=dataset_name, root="../dataset")
+descriptor_fingerprints = [
+    skfp.fingerprints.AutocorrFingerprint,
+    skfp.fingerprints.GETAWAYFingerprint,
+    skfp.fingerprints.MordredFingerprint,
+    skfp.fingerprints.MORSEFingerprint,
+    skfp.fingerprints.RDFFingerprint,
+    skfp.fingerprints.WHIMFingerprint,
+]
+
+LIMIT_SIZE = None
+
+SCRIPT_PATH = os.path.abspath(__file__)
+DATASET_DIR = os.path.join(os.path.dirname(os.path.dirname(SCRIPT_PATH)), "dataset")
+
+
+dataset_params = [
+    ("ogbg-molhiv", "HIV_active"),
+    ("ogbg-molbace", "Class"),
+    ("ogbg-molbbbp", "p_np"),
+]
+
+
+classifier_parameters = [
+    (RandomForestClassifier, {"n_jobs": -1, "class_weight": "balanced"}),
+    (
+        LogisticRegressionCV,
+        {"class_weight": "balanced", "max_iter": 150, "n_jobs": -1},
+    ),
+    (lgb.LGBMClassifier, {"n_jobs": -1, "class_weight": "balanced", "verbose": -1}),
+]
+
+for dataset_name, property_name in dataset_params:
+    dataset = GraphPropPredDataset(name=dataset_name, root=DATASET_DIR)
     split_idx = dataset.get_idx_split()
     train_idx = np.array(split_idx["train"])
     valid_idx = np.array(split_idx["valid"])
     test_idx = np.array(split_idx["test"])
 
+    if LIMIT_SIZE:
+        train_idx = train_idx[:LIMIT_SIZE]
+        valid_idx = valid_idx[:LIMIT_SIZE]
+        test_idx = test_idx[:LIMIT_SIZE]
+
     dataframe = pd.read_csv(
-        f"../dataset/{'_'.join(dataset_name.split('-'))}/mapping/mol.csv.gz"
+        f"{DATASET_DIR}/{'_'.join(dataset_name.split('-'))}/mapping/mol.csv.gz"
     )
 
     print(dataframe.columns)
@@ -54,76 +80,64 @@ for dataset_name, property_name in zip(dataset_names, property_names):
     X_test, y_test = X[test_idx], y[test_idx]
     X_valid, y_valid = X[valid_idx], y[valid_idx]
 
+    X_train_mols = [MolFromSmiles(smiles) for smiles in X_train]
+    X_valid_mols = [MolFromSmiles(smiles) for smiles in X_valid]
+    X_test_mols = [MolFromSmiles(smiles) for smiles in X_test]
+
+    conf_gen = ConformerGenerator(n_jobs=-1, error_on_gen_fail=False)
+    X_train_conf, y_train = conf_gen.transform_x_y(X_train_mols, np.array(y_train))
+    X_valid_conf, y_valid = conf_gen.transform_x_y(X_valid_mols, np.array(y_valid))
+    X_test_conf, y_test = conf_gen.transform_x_y(X_test_mols, np.array(y_test))
+
     records = []
-
-    fp_names = [
-        "ECFP",
-        "Atom Pairs",
-        "Topological Torsion",
-        "MACCS Keys",
-        "ERG",
-        "MAP4",
-        "MHFP",
-    ]
-    fprints = [
-        ECFPFingerprint,
-        AtomPairFingerprint,
-        TopologicalTorsionFingerprint,
-        MACCSKeysFingerprint,
-        ERGFingerprint,
-        MAP4Fingerprint,
-        MHFPFingerprint,
-    ]
-
-    clf_names = ["RF", "LogReg", "LGBM"]
-    classifiers = [
-        RandomForestClassifier,
-        LogisticRegression,
-        lgb.LGBMClassifier,
-    ]
-    classifier_kwargs = [
-        {"n_jobs": -1, "class_weight": "balanced"},
-        {
-            "class_weight": "balanced",
-            "penalty": None,
-            "n_jobs": -1,
-        },
-        {"n_jobs": -1, "class_weight": "balanced", "verbose": 0},
-    ]
 
     np.random.seed(42)
 
-    for fingerprint, fp_name in zip(fprints, fp_names):
+    for fingerprint in fingerprint_classes:
+        fp_name = fingerprint.__name__
         records.append({})
         records[-1]["fp_name"] = fp_name
-        print(fp_name, "Fingerprint")
+        print(fp_name)
         start = time()
         fp_transformer = fingerprint(n_jobs=-1)
-        X_fp_train = fp_transformer.transform(X_train)
-        X_fp_valid = fp_transformer.transform(X_valid)
-        X_fp_test = fp_transformer.transform(X_test)
-
-        scaler = MinMaxScaler()
-        X_fp_train = scaler.fit_transform(X_fp_train)
-        X_fp_valid = scaler.fit_transform(X_fp_valid)
-        X_fp_test = scaler.transform(X_fp_test)
+        X_fp_train = fp_transformer.transform(X_train_conf)
+        X_fp_valid = fp_transformer.transform(X_valid_conf)
+        X_fp_test = fp_transformer.transform(X_test_conf)
 
         end = time()
         execution_time = end - start
         print(f" - Time of fingerprints computing : {round(execution_time,2)}s")
         records[-1]["execution_time"] = execution_time
-        for classifier, clf_name, clf_kwargs in zip(
-            classifiers, clf_names, classifier_kwargs
-        ):
+
+        if fp_transformer in descriptor_fingerprints:
+            imputer = SimpleImputer(strategy="median")
+            X_fp_train = imputer.fit_transform(X_fp_train)
+            X_fp_valid = imputer.transform(X_fp_valid)
+            X_fp_test = imputer.transform(X_fp_test)
+
+            scaler = RobustScaler()
+            X_fp_train = scaler.fit_transform(X_fp_train)
+            X_fp_valid = scaler.transform(X_fp_valid)
+            X_fp_test = scaler.transform(X_fp_test)
+
+        for classifier, clf_kwargs in classifier_parameters:
+            clf_name = classifier.__name__
             scores = []
             scores_valid = []
-            for epoch in range(10):
-                clf = classifier(random_state=epoch, **clf_kwargs)
-                clf.fit(X_fp_train, y_train)
-                scores.append(roc_auc_score(y_test, clf.predict_proba(X_fp_test)[:, 1]))
-                scores_valid.append(
-                    roc_auc_score(y_valid, clf.predict_proba(X_fp_valid)[:, 1])
-                )
+            try:
+                for epoch in range(10):
+                    clf = classifier(random_state=epoch, **clf_kwargs)
+                    clf.fit(X_fp_train, y_train)
+                    scores.append(
+                        roc_auc_score(y_test, clf.predict_proba(X_fp_test)[:, 1])
+                    )
+                    scores_valid.append(
+                        roc_auc_score(y_valid, clf.predict_proba(X_fp_valid)[:, 1])
+                    )
+            except ValueError as e:
+                print(f" - Error: {e}")
+                records[-1][clf_name + "_error"] = str(e)
+                continue
             score = np.average(scores)
             std = np.std(scores)
             score_valid = np.average(scores_valid)
