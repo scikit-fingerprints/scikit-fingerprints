3017bd9f06ad3a22e0e1b17ab69cf8713a6cb0e2
j-adamczyk <jakubadamczyk10@gmail.com>
2024-05-14 21:15:40 +0200

    Add count MACCS

--- tests/fingerprints/maccs.py
+++ tests/fingerprints/maccs.py
@@ -6,7 +6,7 @@ from skfp.fingerprints import MACCSFingerprint
 
 
 def test_maccs_bit_fingerprint(smiles_list, mols_list):
-    maccs_fp = MACCSFingerprint(sparse=False, n_jobs=-1)
+    maccs_fp = MACCSFingerprint(n_jobs=-1)
     X_skfp = maccs_fp.transform(smiles_list)
     X_rdkit = np.array([GetMACCSKeysFingerprint(mol) for mol in mols_list])
 
@@ -14,6 +14,15 @@ def test_maccs_bit_fingerprint(smiles_list, mols_list):
     assert X_skfp.shape == (len(smiles_list), 167)
     assert X_skfp.dtype == np.uint8
     assert np.all(np.isin(X_skfp, [0, 1]))
+
+
+def test_maccs_count_fingerprint(smiles_list, mols_list):
+    maccs_fp = MACCSFingerprint(count=True, n_jobs=-1)
+    X_skfp = maccs_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(smiles_list), 159)
+    assert X_skfp.dtype == np.uint32
+    assert np.all(X_skfp >= 0)
 
 
 def test_maccs_sparse_bit_fingerprint(smiles_list, mols_list):
@@ -25,3 +34,12 @@ def test_maccs_sparse_bit_fingerprint(smiles_list, mols_list):
     assert X_skfp.shape == (len(smiles_list), 167)
     assert X_skfp.dtype == np.uint8
     assert np.all(X_skfp.data == 1)
+
+
+def test_maccs_sparse_count_fingerprint(smiles_list, mols_list):
+    maccs_fp = MACCSFingerprint(count=True, sparse=True, n_jobs=-1)
+    X_skfp = maccs_fp.transform(smiles_list)
+
+    assert X_skfp.shape == (len(smiles_list), 159)
+    assert X_skfp.dtype == np.uint32
+    assert np.all(X_skfp.data >= 0)
