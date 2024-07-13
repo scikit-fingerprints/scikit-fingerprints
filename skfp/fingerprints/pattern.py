7ac7ffe4e11a2beb1f963a893b0589ae1b755d57
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2024-07-13 10:51:30 +0200

    Add Pattern docs (#194)

--- skfp/fingerprints/pattern.py
+++ skfp/fingerprints/pattern.py
@@ -12,7 +12,82 @@ from skfp.utils import ensure_mols
 
 
 class PatternFingerprint(BaseFingerprintTransformer):
-    """Pattern fingerprint."""
+    """
+    Pattern fingerprint.
+
+    The implementation uses RDKit. This is a hashed fingerprint, where
+    fragments are created from very generic SMARTS patterns, taking into
+    consideration only atom and bond types (including bond aromaticity).
+
+    For each pattern, its occurrences are detected and hashed, including
+    atom and bond types involved. The fact that particular pattern matched
+    the molecule at all is also stored by hashing the pattern ID and size.
+
+    Patterns are (last four are too complex to describe easily):
+
+    - [*] (single atom)
+    - [*]~[*] (directly bonded atoms)
+    - [*]~[*]~[*] (three consecutive atoms)
+    - [R]~1~[R]~[R]~1 (3-atom ring)
+    - [R]~1[R]~[R]~[R]~1 (4-atom ring)
+    - [R]~1~[R]~[R]~[R]~[R]~1 (5-atom ring)
+    - [R]~1~[R]~[R]~[R]~[R]~[R]~1 (6-atom ring)
+    - [*]~[*](~[*])~[*] (atom with 3 neighbors)
+    - [*]~[*]~[*](~[*])~[*]
+    - [R](@[R])(@[R])~[R]~[R](@[R])(@[R])
+    - [*]~[R](@[R])@[R](@[R])~[*]
+    - [*]~[R](@[R])@[R]@[R](@[R])~[*]
+
+    Parameters
+    ----------
+    fp_size : int, default=2048
+        Size of output vectors, i.e. number of bits for each fingerprint. Must be
+        positive.
+
+    tautomers : bool, default=False
+        Whether to include tautomeric bonds as a separate bond type when checking
+        pattern matches.
+
+    sparse : bool, default=False
+        Whether to return dense NumPy array, or sparse SciPy CSR array.
+
+    n_jobs : int, default=None
+        The number of jobs to run in parallel. :meth:`transform` is parallelized
+        over the input molecules. ``None`` means 1 unless in a
+        :obj:`joblib.parallel_backend` context. ``-1`` means using all processors.
+        See Scikit-learn documentation on ``n_jobs`` for more details.
+
+    batch_size : int, default=None
+        Number of inputs processed in each batch. ``None`` divides input data into
+        equal-sized parts, as many as ``n_jobs``.
+
+    verbose : int, default=0
+        Controls the verbosity when computing fingerprints.
+
+    Attributes
+    ----------
+    n_features_out : int
+        Number of output features, size of fingerprints. Equal to `fp_size`.
+
+    requires_conformers : bool
+        Whether the fingerprint is 3D-based and requires molecules with conformers as
+        inputs, with ``conf_id`` integer property set. This depends on the ``use_3D``
+        parameter, and has the same value.
+
+    Examples
+    --------
+    >>> from skfp.fingerprints import PatternFingerprint
+    >>> smiles = ["O", "CC", "[C-]#N", "CC=O"]
+    >>> fp = PatternFingerprint()
+    >>> fp
+    PatternFingerprint()
+
+    >>> fp.transform(smiles)
+    array([[0, 0, 0, ..., 0, 0, 0],
+           [0, 0, 0, ..., 0, 0, 0],
+           [0, 0, 0, ..., 0, 0, 0],
+           [0, 0, 0, ..., 0, 0, 0]], dtype=uint8)
+    """
 
     _parameter_constraints: dict = {
         **BaseFingerprintTransformer._parameter_constraints,
