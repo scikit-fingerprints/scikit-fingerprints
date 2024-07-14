7e92e6a57741132b6ceeb89619d2d3fbd88a8ebf
AStaniszewski <staniszewski01adam@gmail.com>
2024-07-14 18:20:14 +0200

    Added scaffold split for SMILES lists

--- skfp/utils/splitters.py
+++ skfp/utils/splitters.py
@@ -1,59 +1,78 @@
-import random
-from collections import defaultdict
-from typing import Optional
+import numpy as np
 from rdkit import Chem
 from rdkit.Chem.Scaffolds import MurckoScaffold
+from typing import Optional
 
 def scaffold_split(smiles_list: list[str],
                    frac_train: float = 0.8,
                    frac_valid: float = 0.1,
                    frac_test: float = 0.1,
-                   seed: Optional[int] = None) -> tuple[list[str], list[str], list[str]]:
+                   seed: Optional[int] = None) -> tuple[
+                       list[int], 
+                       list[int],   
+                       list[int]]:
     """
-    Generates scaffolds for a list of SMILES strings.
+    Splits a list of SMILES into train/val/test using scaffold
 
-    Args:
-    smiles_list (List[str]): A list of SMILES strings.
-    frac_train (float): Fraction of the dataset to be used as the training set.
-    frac_valid (float): Fraction of the dataset to be used as the validation set.
-    frac_test (float): Fraction of the dataset to be used as the test set.
-    seed (Optional[int]): Random seed for reproducibility.
+    TODO: 
+    - Allow the method to accept types other than SMILES string;
+    - Add parameter to configure frqeuency of logging
 
-    Returns:
-    Tuple[List[str], List[str], List[str]]: A tuple containing the training, validation, and test sets.
+    Parameters
+    ----------
+    smiles_list: str 
+        List of SMILES strings to be split.
+    frac_train: float, optional (default 0.8)
+        The fraction of data to be used for the training split.
+    frac_valid: float, optional (default 0.1)
+        The fraction of data to be used for the validation split.
+    frac_test: float, optional (default 0.1)
+        The fraction of data to be used for the test split.
+    seed: int, optional (default None)
+        Random seed to use.
+
+    Returns
+    ----------
+    tuple[list[int], list[int], list[int]]
+        A tuple of train, validation and test indices, each is 
+        a list of ints.
     """
+    np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
+
     if seed is not None:
-        random.seed(seed)
+        np.random.seed(seed=seed)
 
-    scaffolds: dict[str, list[str]] = {}
-    for smile in smiles_list:
-        mol = Chem.MolFromSmiles(smile)
-        if mol is None:
-            continue
-        scaffold = MurckoScaffold.GetScaffoldForMol(mol)
-        scaffold_smiles = Chem.MolToSmiles(scaffold)
-        if scaffold_smiles not in scaffolds:
-            scaffolds[scaffold_smiles] = []
-        scaffolds[scaffold_smiles].append(smile)
+    scaffolds: dict[str, list[int]] = {}
+    data_len: int = len(smiles_list)
+    
+    for ind, smiles in enumerate(smiles_list):
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is not None:
+            scaffold = MurckoScaffold.MurckoScaffoldSmiles(
+                mol=mol,
+                includeChirality=True
+            )
+            if scaffold not in scaffolds:
+                scaffolds[scaffold] = [ind]
+            else:
+                scaffolds[scaffold].append(ind)
 
-    scaffold_sets = list(scaffolds.values())
-    random.shuffle(scaffold_sets)
+    scaffold_sets: list[list[int]] = [scaffold_set for (scaffold, scaffold_set) 
+                                    in sorted(scaffolds.items(), key=lambda x: len(x[1]), reverse=True)]
 
-    n_total: int = len(smiles_list)
-    n_train: int = int(frac_train * n_total)
-    n_valid : int = len(frac_valid * n_total)
-    n_test = n_total - n_train - n_valid
-
-    train_set: list[str] = []
-    valid_set: list[str] = []
-    test_set: list[str] = []
+    train_cutoff: int = frac_train * len(smiles_list)
+    valid_cutoff: int = train_cutoff + frac_valid * len(smiles_list)
+    train_ids: list[int] = []
+    valid_ids: list[int] = []
+    test_ids: list[int] = []
 
     for scaffold_set in scaffold_sets:
-        if len(train_set) + len(scaffold_set) <= n_train:
-            train_set.extend(scaffold_set)
-        elif len(valid_set) + len(scaffold_set) <= n_valid:
-            valid_set.extend(scaffold_set)
+        if len(train_ids) + len(scaffold_set) > train_cutoff:
+            if len(train_ids) + len(valid_ids) + len(scaffold_set) > valid_cutoff:
+                test_ids += scaffold_set
+            else:
+                valid_ids += scaffold_set
         else:
-            test_set.extend(scaffold_set)
+            train_ids += scaffold_set
 
-    return train_set, valid_set, test_set
+    return train_ids, valid_ids, test_ids
