f95a949a55b9073c67331ce1e75d45faf0bff47e
AStaniszewski <staniszewski01adam@gmail.com>
2024-07-15 19:48:19 +0200

    Added scaffold split

--- skfp/utils/splitters.py
+++ skfp/utils/splitters.py
@@ -1,27 +1,25 @@
+from collections.abc import Sequence
 import numpy as np
+from typing import Optional, Any
+
 from rdkit import Chem
 from rdkit.Chem.Scaffolds import MurckoScaffold
-from typing import Optional
 
-def scaffold_split(smiles_list: list[str],
+from validators import ensure_mols
+
+def scaffold_split(molecules: Sequence[Any],
                    frac_train: float = 0.8,
                    frac_valid: float = 0.1,
                    frac_test: float = 0.1,
-                   seed: Optional[int] = None) -> tuple[
-                       list[int], 
-                       list[int],   
-                       list[int]]:
+                   seed: Optional[int] = None) -> tuple[list[int], list[int],  list[int]]:
+    
     """
-    Splits a list of SMILES into train/val/test using scaffold
-
-    TODO: 
-    - Allow the method to accept types other than SMILES string;
-    - Add parameter to configure frqeuency of logging
+    Splits a list of SMILES or MOL objects into train/val/test using scaffold
 
     Parameters
     ----------
-    smiles_list: str 
-        List of SMILES strings to be split.
+    molecules: str 
+        Sequence representing either SMILES of Mol
     frac_train: float, optional (default 0.8)
         The fraction of data to be used for the training split.
     frac_valid: float, optional (default 0.1)
@@ -37,31 +35,31 @@ def scaffold_split(smiles_list: list[str],
         A tuple of train, validation and test indices, each is 
         a list of ints.
     """
+    molecules = ensure_mols(molecules)
+
     np.testing.assert_almost_equal(frac_train + frac_valid + frac_test, 1.0)
 
     if seed is not None:
         np.random.seed(seed=seed)
 
     scaffolds: dict[str, list[int]] = {}
-    data_len: int = len(smiles_list)
     
-    for ind, smiles in enumerate(smiles_list):
-        mol = Chem.MolFromSmiles(smiles)
-        if mol is not None:
-            scaffold = MurckoScaffold.MurckoScaffoldSmiles(
-                mol=mol,
-                includeChirality=True
-            )
-            if scaffold not in scaffolds:
-                scaffolds[scaffold] = [ind]
-            else:
-                scaffolds[scaffold].append(ind)
+    for ind, molecule in enumerate(molecules):
+        scaffold = MurckoScaffold.MurckoScaffoldSmiles(
+            mol=molecule,
+            includeChirality=True
+        )
+        if scaffold not in scaffolds:
+            scaffolds[scaffold] = [ind]
+        else:
+            scaffolds[scaffold].append(ind)
 
-    scaffold_sets: list[list[int]] = [scaffold_set for (scaffold, scaffold_set) 
-                                    in sorted(scaffolds.items(), key=lambda x: len(x[1]), reverse=True)]
+    scaffold_sets = [scaffold_set for (_, scaffold_set) 
+                    in sorted(scaffolds.items(), key=lambda x: len(x[1]), reverse=True)]
 
-    train_cutoff: int = frac_train * len(smiles_list)
-    valid_cutoff: int = train_cutoff + frac_valid * len(smiles_list)
+
+    train_cutoff: int = frac_train * len(molecules)
+    valid_cutoff: int = train_cutoff + frac_valid * len(molecules)
     train_ids: list[int] = []
     valid_ids: list[int] = []
     test_ids: list[int] = []
