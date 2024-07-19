6a334c42ecb2523148fd34deeea33fc083e32dc9
j-adamczyk <jakubadamczyk10@gmail.com>
2024-07-19 13:53:09 +0200

    Add optimized hyperparameter tuning

--- skfp/model_selection/grid_search.py
+++ skfp/model_selection/grid_search.py
@@ -1,6 +1,8 @@
 from collections.abc import Sequence
+from time import time
 from typing import Union
 
+import joblib.logger
 import numpy as np
 from rdkit.Chem import Mol
 from scipy.sparse import csr_array
@@ -29,8 +31,16 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
     3. Optimize estimator hyperparameters
 
     This way, computer fingerprint representations are efficiently used for many sets of
-    estimator hyperparameters. The difference is particularly significant for more
-    computationally fingerprints and large grids for estimators.
+    estimator hyperparameters. This is useful when tuning classifier or fingerprint and
+    classifier. When only fingerprint is tuned, combination of `Pipeline` and `GridSearchCV`
+    is enough. The difference is particularly significant for more computationally
+    fingerprints and large grids for estimators.
+
+    Note that much of the behavior is controlled via passed `estimator_cv` object, e.g.
+    the `scoring` metric used to select the best pipeline. In particular, if `GridSearchCV`
+    is used, then the result is equivalent to using grid search on all hyperparameters,
+    but faster. However, any other strategy can be used for the estimator, e.g.
+    `RandomizedSearchCV`.
 
     Parameters
     ----------
@@ -39,15 +49,21 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
         `n_jobs` larger than 1, since parallelization is not performed here when going
         through fingerprint hyperparameter grid.
 
-    estimator_cv : object
-        Inner cross-validation object for tuning estimator, e.g. `GridSearchCV`. Should
-        be an instantiated object, not a class.
-
     fp_param_grid : dict or list[dict]
         Dictionary with fingerprint hyperparameters names (`str`) as keys and lists of
         hyperparameter settings to try as values, or a list of such dictionaries, in which
         case the grids spanned by each dictionary in the list are explored. This enables
         searching over any sequence of hyperparameter settings.
+
+    estimator_cv : object
+        Inner cross-validation object for tuning estimator, e.g. `GridSearchCV`. Should
+        be an instantiated object, not a class.
+
+    verbose : int, default=0
+        Controls the verbosity: the higher, the more messages.
+
+        - >0 : size of parameter grid, parameter candidate for each fold
+        - >1 : the computation time and score for each candidate
 
     Attributes
     ----------
@@ -83,7 +99,6 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
     >>> from skfp.model_selection import FingerprintEstimatorGridSearch
     >>> from sklearn.ensemble import RandomForestClassifier
     >>> from sklearn.model_selection import GridSearchCV
-    >>> from sklearn.svm import LinearSVC
     >>> smiles, labels = load_bace()
     >>> fp = ECFPFingerprint(n_jobs=-1)
     >>> fp_params = {"radius": [1, 2, 3]}
@@ -105,19 +120,22 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
 
     _parameter_constraints: dict = {
         "fingerprint": [BaseFingerprintTransformer],
-        "estimator_cv": [BaseSearchCV],
         "fp_param_grid": [dict, list],
+        "estimator_cv": [BaseSearchCV],
+        "verbose": ["verbose"],
     }
 
     def __init__(
         self,
         fingerprint: BaseFingerprintTransformer,
-        estimator_cv: BaseSearchCV,
         fp_param_grid: Union[dict, list[dict]],
+        estimator_cv: BaseSearchCV,
+        verbose: int = 0,
     ):
         self.fingerprint = fingerprint
-        self.estimator_cv = estimator_cv
         self.fp_param_grid = fp_param_grid
+        self.estimator_cv = estimator_cv
+        self.verbose = verbose
 
     def _validate_params(self):
         super()._validate_params()
@@ -132,14 +150,27 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
         self.best_score_ = -1  # in scikit-learn, higher score is always better
         self.best_estimator_cv_: BaseSearchCV = None  # type: ignore
 
-        for fp_params in ParameterGrid(self.fp_param_grid):
+        param_grid = ParameterGrid(self.fp_param_grid)
+        param_grid_size = len(param_grid)
+        if self.verbose:
+            print(f"Fitting {param_grid_size} candidate hyperparameter sets.")
+
+        for idx, fp_params in enumerate(param_grid):
+            if self.verbose:
+                self._print_start_msg(
+                    curr_idx=idx + 1,
+                    grid_size=param_grid_size,
+                    curr_params=fp_params,
+                )
+            start_time = time()
+
             fp: BaseFingerprintTransformer = clone(self.fingerprint)
-            fp.set_params(**fp_params)
+            fp.set_params(**clone(fp_params, safe=False))
 
             X_fp = fp.fit_transform(X, y, **params)
 
             curr_cv = clone(self.estimator_cv)
-            curr_cv.fit(X_fp, y)
+            curr_cv.fit(X_fp, y, **params)
             curr_score = curr_cv.best_score_
             estimator_params = curr_cv.best_params_
 
@@ -151,6 +182,17 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
                 self.best_fp_params_ = fp_params
                 self.best_score_ = curr_score
                 self.best_estimator_cv_ = curr_cv
+
+            end_time = time()
+            if self.verbose > 1:
+                self._print_end_msg(
+                    curr_idx=idx + 1,
+                    grid_size=param_grid_size,
+                    curr_params=fp_params,
+                    start_time=start_time,
+                    end_time=end_time,
+                    curr_score=curr_score,
+                )
 
         return self
 
@@ -164,3 +206,32 @@ class FingerprintEstimatorGridSearch(BaseEstimator):
 
     def transform(self, X: Sequence[Union[str, Mol]]) -> Union[np.ndarray, csr_array]:
         return self.best_fp_.transform(X)
+
+    def _print_start_msg(
+        self, curr_idx: int, grid_size: int, curr_params: dict
+    ) -> None:
+        progress_msg = f"{curr_idx}/{grid_size}"
+        params_msg = ", ".join(
+            f"{name}={curr_params[name]}" for name in sorted(curr_params)
+        )
+        start_msg = f"[{progress_msg}] START {params_msg}"
+        print(f"{start_msg}{(80 - len(start_msg)) * '.'}")
+
+    def _print_end_msg(
+        self,
+        curr_idx: int,
+        grid_size: int,
+        curr_params: dict,
+        start_time: float,
+        end_time: float,
+        curr_score: float,
+    ) -> None:
+        progress_msg = f"{curr_idx}/{grid_size}"
+        params_msg = ", ".join(
+            f"{name}={curr_params[name]}" for name in sorted(curr_params)
+        )
+        score_msg = f"score={curr_score:.3f}"
+        total_time = joblib.logger.short_format_time(end_time - start_time)
+        time_msg = f"total time={total_time}"
+        end_msg = f"[{progress_msg}] END {params_msg}; {score_msg}; {time_msg}"
+        print(f"{end_msg}{(80 - len(end_msg)) * '.'}")
