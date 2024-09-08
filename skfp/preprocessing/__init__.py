816095a287b7480e6a5e5047ac3101240e47030d
j-adamczyk <jakub.adamczyk@placewise.com>
2024-09-08 12:42:17 +0200

    Add BMS filter

--- skfp/preprocessing/__init__.py
+++ skfp/preprocessing/__init__.py
@@ -1,6 +1,14 @@
 """Classes for preprocessing molecules."""
 
 from .conformer_generator import ConformerGenerator
-from .filters import BrenkFilter, LipinskiFilter, NIHFilter, PAINSFilter, ZINCFilter
+from .filters import (
+    BMSFilter,
+    BrenkFilter,
+    GlaxoFilter,
+    LipinskiFilter,
+    NIHFilter,
+    PAINSFilter,
+    ZINCFilter,
+)
 from .mol_to_from_smiles import MolFromSmilesTransformer, MolToSmilesTransformer
 from .standardization import MolStandardizer
--- skfp/preprocessing/filters/__init__.py
+++ skfp/preprocessing/filters/__init__.py
@@ -1,4 +1,6 @@
+from .bms import BMSFilter
 from .brenk import BrenkFilter
+from .glaxo import GlaxoFilter
 from .lipinski import LipinskiFilter
 from .nih import NIHFilter
 from .pains import PAINSFilter
