1b62eaa842e0e48c3c887e5f07cddbda4eec5e0c
j-adamczyk <jakub.adamczyk@placewise.com>
2024-09-23 18:29:14 +0200

    Fix docs

--- skfp/model_selection/__init__.py
+++ skfp/model_selection/__init__.py
@@ -5,6 +5,8 @@ from .hyperparam_search import (
     FingerprintEstimatorRandomizedSearch,
 )
 from .splitters import (
+    butina_train_test_split,
+    butina_train_valid_test_split,
     randomized_scaffold_train_test_split,
     randomized_scaffold_train_valid_test_split,
     scaffold_train_test_split,
--- skfp/model_selection/splitters/__init__.py
+++ skfp/model_selection/splitters/__init__.py
@@ -1,3 +1,4 @@
+from .butina_split import butina_train_test_split, butina_train_valid_test_split
 from .randomized_scaffold_split import (
     randomized_scaffold_train_test_split,
     randomized_scaffold_train_valid_test_split,
