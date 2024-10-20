9b54f920ea1cc78a28ba4a87ff1f7befa397360a
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-10-20 18:19:09 +0200

    Add docstrings

--- skfp/fingerprints/rdkit_2d_desc.py
+++ skfp/fingerprints/rdkit_2d_desc.py
@@ -140,6 +140,25 @@ class RDKit2DDescriptorsFingerprint(BaseFingerprintTransformer):
             return np.array(X, dtype=np.float32)
 
     def get_feature_names_out(self, input_features=None):
+        """Get output feature names for transformation.
+
+        Parameters
+        ----------
+        input_features : array-like of str or None, default=None
+            Input features.
+
+            - If `input_features` is `None`, then `feature_names_in_` is
+              used as feature names in. If `feature_names_in_` is not defined,
+              then the following input feature names are generated:
+              `["x0", "x1", ..., "x(n_features_in_ - 1)"]`.
+            - If `input_features` is an array-like, then `input_features` must
+              match `feature_names_in_` if `feature_names_in_` is defined.
+
+        Returns
+        -------
+        feature_names_out : ndarray of str objects
+            Same as input features.
+        """
         super().get_feature_names_out(input_features)
 
         from descriptastorus.descriptors.rdDescriptors import RDKit2D
