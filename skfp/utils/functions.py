a993807c1addbf08f772fc5cfbbb7b079a3706bb
my-alaska <piotrztych@wp.pl>
2024-11-09 22:45:43 +0100

    fix data retrieval

--- skfp/utils/functions.py
+++ skfp/utils/functions.py
@@ -8,6 +8,6 @@ def get_data_from_indices(data: Sequence, indices: Sequence[int]) -> list:
     Helper function to retrieve data elements from specified indices.
     """
     if isinstance(data, (pd.Series, pd.DataFrame)):
-        return [data.iloc[idx] for idx in set(indices)]
+        return [data.iloc[idx] for idx in indices]
     else:
-        return [data[idx] for idx in set(indices)]
+        return [data[idx] for idx in indices]
