4bcf1a75ecfbc2056976dbdf941f5263b222b763
Martin Fitzner <martin.fitzner@merckgroup.com>
2024-11-10 22:29:03 +0100

    Remove ignored parameter, update docstrings

--- skfp/fingerprints/mordred_fp.py
+++ skfp/fingerprints/mordred_fp.py
@@ -122,27 +122,14 @@ class MordredFingerprint(BaseFingerprintTransformer):
             else np.array(X, dtype=np.float32)
         )
 
-    def get_feature_names_out(self, input_features=None):
+    def get_feature_names_out(self):
         """Get output feature names for transformation.
-
-        Parameters
-        ----------
-        input_features : array-like of str or None, default=None
-            Input features.
-
-            - If `input_features` is `None`, then `feature_names_in_` is
-              used as feature names in. If `feature_names_in_` is not defined,
-              then the following input feature names are generated:
-              `["x0", "x1", ..., "x(n_features_in_ - 1)"]`.
-            - If `input_features` is an array-like, then `input_features` must
-              match `feature_names_in_` if `feature_names_in_` is defined.
 
         Returns
         -------
         feature_names_out : ndarray of str objects
-            Same as input features.
+            Names of the Mordred descriptors.
         """
-        super().get_feature_names_out(input_features)
         calc = Calculator(descriptors, ignore_3D=not self.use_3D)
 
         return np.asarray([str(d) for d in calc.descriptors], dtype=object)
