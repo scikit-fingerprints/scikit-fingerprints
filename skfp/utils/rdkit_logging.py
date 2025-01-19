60ffd6a4df5b51bd8d047dee002f6fd1bb24b839
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2025-01-19 23:14:06 +0100

    Add missing backtick

--- skfp/utils/rdkit_logging.py
+++ skfp/utils/rdkit_logging.py
@@ -6,7 +6,7 @@ from rdkit.rdBase import BlockLogs, DisableLog, EnableLog
 @contextmanager
 def no_rdkit_logs(suppress_warnings: bool = False):
     """
-    Context manager that disables RDKit logging. If ``suppress_warnings` is True,
+    Context manager that disables RDKit logging. If ``suppress_warnings`` is True,
     also blocks all warnings and info messages.
     """
     try:
