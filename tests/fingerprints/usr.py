8746f3535891f479471f916374e70f7a4a2d8750
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2025-02-09 11:41:30 +0100

    Fix tests

--- tests/fingerprints/usr.py
+++ tests/fingerprints/usr.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 import pytest
 from rdkit.Chem.rdMolDescriptors import GetUSR
@@ -14,7 +12,7 @@ def mols_conformers_3_plus_atoms(mols_conformers_list):
 
 
 def test_usr_bit_fingerprint(mols_conformers_3_plus_atoms):
-    usr_fp = USRFingerprint(n_jobs=-1)
+    usr_fp = USRFingerprint()
     X_skfp = usr_fp.transform(mols_conformers_3_plus_atoms)
 
     X_rdkit = np.array(
@@ -24,15 +22,7 @@ def test_usr_bit_fingerprint(mols_conformers_3_plus_atoms):
         ]
     )
 
-    # on macOS for USR and USRCAT we get slightly different results in skfp and RDKit
-    # debugging for a long time didn't help, so we check just basic statistic instead
-    if sys.platform == "darwin":
-        assert np.isclose(np.min(X_skfp), np.min(X_rdkit), atol=1e-3)
-        assert np.isclose(np.mean(X_skfp), np.mean(X_rdkit), atol=1e-3)
-        assert np.isclose(np.max(X_skfp), np.max(X_rdkit), atol=1e-3)
-    else:
-        assert np.allclose(X_skfp, X_rdkit, atol=1e-3)
-
+    assert np.allclose(X_skfp, X_rdkit, atol=1e-3)
     assert X_skfp.shape == (len(mols_conformers_3_plus_atoms), 12)
     assert np.issubdtype(X_skfp.dtype, np.floating)
 
@@ -40,7 +30,7 @@ def test_usr_bit_fingerprint(mols_conformers_3_plus_atoms):
 def test_usr_bit_fingerprint_transform_x_y(mols_conformers_3_plus_atoms):
     labels = np.arange(len(mols_conformers_3_plus_atoms))
 
-    usr_fp = USRFingerprint(n_jobs=-1)
+    usr_fp = USRFingerprint()
     X_skfp, y_skfp = usr_fp.transform_x_y(mols_conformers_3_plus_atoms, labels)
 
     X_rdkit = []
@@ -53,15 +43,7 @@ def test_usr_bit_fingerprint_transform_x_y(mols_conformers_3_plus_atoms):
     X_rdkit = np.array(X_rdkit)
     y_rdkit = np.array(y_rdkit)
 
-    # on macOS for USR and USRCAT we get slightly different results in skfp and RDKit
-    # debugging for a long time didn't help, so we check just basic statistic instead
-    if sys.platform == "darwin":
-        assert np.isclose(np.min(X_skfp), np.min(X_rdkit), atol=1e-3)
-        assert np.isclose(np.mean(X_skfp), np.mean(X_rdkit), atol=1e-3)
-        assert np.isclose(np.max(X_skfp), np.max(X_rdkit), atol=1e-3)
-    else:
-        assert np.allclose(X_skfp, X_rdkit, atol=1e-3)
-
+    assert np.allclose(X_skfp, X_rdkit, atol=1e-3)
     assert X_skfp.shape == (len(mols_conformers_3_plus_atoms), 12)
     assert np.issubdtype(X_skfp.dtype, np.floating)
     assert np.array_equal(y_skfp, y_rdkit)
