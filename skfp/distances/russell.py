652801313b927dfc17222b7863afd5eca0933cd8
AStaniszewski <staniszewski01adam@gmail.com>
2025-02-26 20:38:34 +0100

    Add Russell distance and similarity

---
+++ skfp/distances/russell.py
@@ -0,0 +1,202 @@
+from typing import Union
+
+import numpy as np
+from scipy.sparse import csr_array
+from sklearn.utils._param_validation import validate_params
+
+
+@validate_params(
+    {
+        "vec_a": [
+            "array-like",
+            csr_array,
+        ],
+        "vec_b": [
+            "array-like",
+            csr_array,
+        ],
+    },
+    prefer_skip_nested_validation=True,
+)
+def russell_binary_similarity(
+    vec_a: Union[
+        np.ndarray,
+        csr_array,
+    ],
+    vec_b: Union[
+        np.ndarray,
+        csr_array,
+    ],
+) -> float:
+    r"""
+    Russell similarity for vectors of binary values.
+
+    Computes the Russell similarity [1]_ [2]_ [3]_ for binary data between two
+    input arrays or sparse matrices, using the formula:
+
+    .. math::
+
+        sim(x, y) = \frac{a}{n}
+
+    where
+
+    - :math:`a` - common "on" bits.
+    - :math:`n` - length of passed vectors
+
+    The calculated similarity falls within the range :math:`[0, 1]`.
+    Passing all-zero vectors to this function results in a similarity of 0.
+    Passing empty vectors results in similarity of 0.
+
+    Parameters
+    ----------
+    vec_a : {ndarray, sparse matrix}
+        First binary input array or sparse matrix.
+
+    vec_b : {ndarray, sparse matrix}
+        Second binary input array or sparse matrix.
+
+    Returns
+    -------
+    similarity : float
+        Russell similarity between ``vec_a`` and ``vec_b``.
+
+    References
+    ----------
+    .. [1] `Russell P.F., Rao T.R.
+        "On habitat and association of species of anopheline larvae in south-eastern Madras. (1940)"
+        <https://www.cabidigitallibrary.org/doi/full/10.5555/19412900343>`_
+
+    .. [2] `Deza M.M., Deza E.
+        "Encyclopedia of Distances."
+        Springer, Berlin, Heidelberg, 2009.
+        <https://doi.org/10.1007/978-3-642-00234-2_1>`_
+
+    .. [3] `RDKit documentation
+        <https://www.rdkit.org/docs/source/rdkit.DataStructs.cDataStructs.html>`_
+
+    Examples
+    --------
+    >>> from skfp.distances import russell_binary_similarity
+    >>> import numpy as np
+    >>> vec_a = np.array([1, 1, 1])
+    >>> vec_b = np.array([1, 1, 1])
+    >>> sim = russell_binary_similarity(vec_a, vec_b)
+    >>> sim
+    1.0
+
+    >>> from scipy.sparse import csr_array
+    >>> vec_a = csr_array([[1, 1, 1]])
+    >>> vec_b = csr_array([[1, 1, 1]])
+    >>> sim = russell_binary_similarity(vec_a, vec_b)
+    >>> sim
+    1.0
+    """
+    if type(vec_a) is not type(vec_b):
+        raise TypeError(
+            f"Both vec_a and vec_b must be of the same type, "
+            f"got {type(vec_a)} and {type(vec_b)}"
+        )
+
+    if np.sum(vec_a) == 0 == np.sum(vec_b):
+        return 1.0
+
+    if isinstance(vec_a, np.ndarray):
+        a = np.sum(np.logical_and(vec_a, vec_b))
+        n = len(vec_a)
+    else:
+        n = vec_a.shape[1]
+        vec_a_idxs = set(vec_a.indices)
+        vec_b_idxs = set(vec_b.indices)
+
+        a = len(vec_a_idxs & vec_b_idxs)
+
+    # Empty vectors
+    if n == 0:
+        return 0.0
+
+    russell_sim = a / n
+
+    return float(russell_sim)
+
+
+@validate_params(
+    {
+        "vec_a": [
+            "array-like",
+            csr_array,
+        ],
+        "vec_b": [
+            "array-like",
+            csr_array,
+        ],
+    },
+    prefer_skip_nested_validation=True,
+)
+def russell_binary_distance(
+    vec_a: Union[
+        np.ndarray,
+        csr_array,
+    ],
+    vec_b: Union[
+        np.ndarray,
+        csr_array,
+    ],
+) -> float:
+    """
+    Russell distance for vectors of binary values.
+
+    Computes the Russell distance [1]_ [2]_ [3]_ for binary data between two
+    input arrays or sparse matrices by subtracting the similarity from 1,
+    using the formula:
+
+    .. math::
+        dist(a, b) = 1 - sim(a, b)
+
+    The calculated similarity falls within the range :math:`[0, 1]`.
+    Passing all-zero vectors to this function results in a similarity of 1.
+
+    Parameters
+    ----------
+    vec_a : {ndarray, sparse matrix}
+        First binary input array or sparse matrix.
+
+    vec_b : {ndarray, sparse matrix}
+        Second binary input array or sparse matrix.
+
+    Returns
+    -------
+    distance : float
+        Russell distance between ``vec_a`` and ``vec_b``.
+
+    References
+    ----------
+    .. [1] `Russell P.F., Rao T.R.
+        "On habitat and association of species of anopheline larvae in south-eastern Madras. (1940)"
+        <https://www.cabidigitallibrary.org/doi/full/10.5555/19412900343>`_
+
+    .. [2] `Deza M.M., Deza E.
+        "Encyclopedia of Distances."
+        Springer, Berlin, Heidelberg, 2009.
+        <https://doi.org/10.1007/978-3-642-00234-2_1>`_
+
+    .. [3] `RDKit documentation
+        <https://www.rdkit.org/docs/source/rdkit.DataStructs.cDataStructs.html>`_
+
+    Examples
+    --------
+    >>> from skfp.distances import russell_binary_distance
+    >>> import numpy as np
+    >>> vec_a = np.array([1, 1, 1])
+    >>> vec_b = np.array([1, 1, 1])
+    >>> dist = russell_binary_distance(vec_a, vec_b)
+    >>> dist
+    0.0
+
+    >>> from scipy.sparse import csr_array
+    >>> vec_a = csr_array([[1, 1, 1]])
+    >>> vec_b = csr_array([[1, 1, 1]])
+    >>> dist = russell_binary_distance(vec_a, vec_b)
+    >>> dist
+    0.0
+    """
+    return 1 - russell_binary_similarity(vec_a, vec_b)
---
+++ tests/distances/russell.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pytest
+from scipy.sparse import csr_array
+
+from skfp.distances import russell_binary_distance, russell_binary_similarity
+from tests.distances.utils import (
+    assert_distance_values,
+    assert_similarity_values,
+)
+
+
+def _get_values() -> list[tuple[list[int], list[int], str, float, float]]:
+    # vec_a, vec_b, comparison, similarity, distance
+    return [
+        ([1, 0, 0], [0, 1, 1], "==", 0.0, 1.0),
+        ([1, 0, 0], [0, 0, 0], "==", 0.0, 1.0),
+        ([0, 0, 0], [0, 0, 0], "==", 1.0, 0.0),
+        ([1, 1, 1], [1, 1, 1], "==", 1.0, 0.0),
+        ([0, 0, 0], [0, 0, 0], "==", 1.0, 0.0),
+        ([1, 0, 0], [1, 0, 0], "==", 1 / 3, 2 / 3),
+        ([1, 1, 1], [1, 1, 1], "==", 1.0, 0.0),
+    ]
+
+
+@pytest.mark.parametrize(
+    "vec_a, vec_b, comparison, similarity, distance", _get_values()
+)
+def test_russell(vec_a, vec_b, comparison, similarity, distance):
+    vec_a = np.array(vec_a)
+    vec_b = np.array(vec_b)
+
+    vec_a_sparse = csr_array([vec_a])
+    vec_b_sparse = csr_array([vec_b])
+
+    sim_dense = russell_binary_similarity(vec_a, vec_b)
+    dist_dense = russell_binary_distance(vec_a, vec_b)
+
+    sim_sparse = russell_binary_similarity(vec_a_sparse, vec_b_sparse)
+    dist_sparse = russell_binary_distance(vec_a_sparse, vec_b_sparse)
+
+    assert_similarity_values(sim_dense, comparison, similarity)
+    assert_similarity_values(sim_sparse, comparison, similarity)
+
+    assert_distance_values(dist_dense, comparison, distance)
+    assert_distance_values(dist_sparse, comparison, distance)
+
+    assert np.isclose(sim_dense, sim_sparse)
+    assert np.isclose(dist_dense, dist_sparse)
