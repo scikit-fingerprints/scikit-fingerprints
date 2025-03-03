bcff6dbd5efcd5ee816a5356d1571fb24842d79d
AStaniszewski <staniszewski01adam@gmail.com>
2025-03-03 20:25:11 +0100

    Fix dosc examples

--- skfp/distances/tanimoto.py
+++ skfp/distances/tanimoto.py
@@ -351,7 +351,7 @@ def bulk_tanimoto_binary_similarity(
     >>> vec_b = np.array([[1, 0, 1], [0, 1, 1]])
     >>> sim = bulk_tanimoto_binary_similarity(vec_a, vec_b)
     >>> sim
-    [[1.0, 1.0], [1.0, 1.0]]
+    [[1.0, 0.5], [0.5, 1.0]]
     """
     if Y is None:
         return _bulk_tanimoto_binary_similarity_single(X)
@@ -445,12 +445,12 @@ def bulk_tanimoto_binary_distance(
     >>> Y = np.array([[1, 0, 1], [1, 0, 1]])
     >>> dist = bulk_tanimoto_binary_distance(X, Y)
     >>> dist
-    [[1, 1], [1, 1]]
+    [[0, 0], [0, 0]]
 
     >>> X = np.array([[1, 0, 1], [1, 0, 1]])
     >>> dist = bulk_tanimoto_binary_distance(X)
     >>> dist
-    [[1, 1], [1, 1]]
+    [[0, 0], [0, 0]]
     """
     return 1 - bulk_tanimoto_binary_similarity(X, Y)
 
@@ -499,7 +499,7 @@ def bulk_tanimoto_count_similarity(
     >>> vec_b = np.array([[1, 0, 1], [0, 1, 1]])
     >>> sim = bulk_tanimoto_count_similarity(vec_a, vec_b)
     >>> sim
-    [[1.0, 0.5], [0.5, 1.0]]
+    [[1., 0.5], [0.5, 1.]]
     """
     X = X.astype(float)  # Numba does not allow integers
 
