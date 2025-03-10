d41a1675bd7525080c645d626a521a312208751a
Adam Staniszewski <119704270+StaniszewskiA@users.noreply.github.com>
2025-03-10 21:05:22 +0100

    Bulk Russell sim and dist (#425)

--- tests/distances/russell.py
+++ tests/distances/russell.py
@@ -3,6 +3,11 @@ import pytest
 from scipy.sparse import csr_array
 
 from skfp.distances import russell_binary_distance, russell_binary_similarity
+from skfp.distances.russell import (
+    bulk_russell_binary_distance,
+    bulk_russell_binary_similarity,
+)
+from skfp.fingerprints.ecfp import ECFPFingerprint
 from tests.distances.utils import (
     assert_distance_values,
     assert_similarity_values,
@@ -46,3 +51,32 @@ def test_russell(vec_a, vec_b, comparison, similarity, distance):
 
     assert np.isclose(sim_dense, sim_sparse)
     assert np.isclose(dist_dense, dist_sparse)
+
+
+def test_bulk_russell_binary(mols_list):
+    fp = ECFPFingerprint()
+    fps = fp.transform(mols_list[:10])
+
+    pairwise_sim = [
+        [russell_binary_similarity(fps[i], fps[j]) for j in range(len(fps))]
+        for i in range(len(fps))
+    ]
+    pairwise_dist = [
+        [russell_binary_distance(fps[i], fps[j]) for j in range(len(fps))]
+        for i in range(len(fps))
+    ]
+
+    bulk_sim = bulk_russell_binary_similarity(fps)
+    bulk_dist = bulk_russell_binary_distance(fps)
+
+    assert np.allclose(pairwise_sim, bulk_sim)
+    assert np.allclose(pairwise_dist, bulk_dist)
+
+
+def test_bulk_russell_second_array(mols_list):
+    fp = ECFPFingerprint()
+    fps = fp.transform(mols_list[:10])
+
+    bulk_sim_single = bulk_russell_binary_similarity(fps)
+    bulk_sim_two = bulk_russell_binary_similarity(fps, fps)
+    assert np.allclose(bulk_sim_single, bulk_sim_two)
