8a03e55cd1991bf4c4c12a44eb48d51a2d14c364
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2025-05-18 14:42:57 +0200

    Add Hotelling T^2 test

--- skfp/applicability_domain/__init__.py
+++ skfp/applicability_domain/__init__.py
@@ -1,4 +1,5 @@
 from .bounding_box import BoundingBoxADChecker
 from .convex_hull import ConvexHullADChecker
+from .hotelling_t2_test import HotellingT2TestADChecker
 from .leverage import LeverageADChecker
 from .pca_bounding_box import PCABoundingBoxADChecker
---
+++ skfp/applicability_domain/hotelling_t2_test.py
@@ -0,0 +1,127 @@
+from typing import Optional, Union
+
+import numpy as np
+from scipy.sparse import csr_array
+from scipy.stats import f as f_dist
+from sklearn.utils._param_validation import Interval, RealNotInt
+from sklearn.utils.validation import check_is_fitted, validate_data
+
+from skfp.bases.base_ad_checker import BaseADChecker
+
+
+class HotellingT2TestADChecker(BaseADChecker):
+    r"""
+    Hotelling's T^2 test method.
+
+    TODO description
+    This approach measures the Mahalanobis distance of a new sample from the mean
+    of the training data, scaled by the covariance structure of the training data.
+
+    Typically, physicochemical properties (continous features) are used as inputs.
+    Consider scaling, normalizing, or transforming them before computing AD to lessen
+    effects of outliers, e.g. with ``PowerTransformer`` or ``RobustScaler``. In case
+    of Hotelling's T^2 test, using PCA beforehand to obtain orthogonal features is
+    particularly beneficial.
+
+    TODO scaling
+
+    Parameters
+    ----------
+    alpha: float, default=0.1
+        Statistical test significance level, in range ``[0, 1]``. Lower values are
+        more conservative and correspond to a smaller applicability domain.
+
+    n_jobs : int, default=None
+        The number of jobs to run in parallel. :meth:`transform_x_y` and
+        :meth:`transform` are parallelized over the input molecules. ``None`` means 1
+        unless in a :obj:`joblib.parallel_backend` context. ``-1`` means using all
+        processors. See scikit-learn documentation on ``n_jobs`` for more details.
+
+    verbose : int or dict, default=0
+        Controls the verbosity when filtering molecules.
+        If a dictionary is passed, it is treated as kwargs for ``tqdm()``,
+        and can be used to control the progress bar.
+
+    References
+    ----------
+    .. [1]
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skfp.applicability_domain import PCABoundingBoxADChecker
+    >>> X_train = np.array([[0.0, 1.0], [0.0, 3.0], [3.0, 1.0]])
+    >>> X_test = np.array([[1.0, 1.0], [1.0, 2.0], [20.0, 3.0]])
+    >>> pca_bb_ad_checker = PCABoundingBoxADChecker()
+    >>> pca_bb_ad_checker
+    PCABoundingBoxADChecker()
+
+    >>> pca_bb_ad_checker.fit(X_train)
+    PCABoundingBoxADChecker()
+
+    >>> pca_bb_ad_checker.predict(X_test)
+    array([ True,  True, False])
+    """
+
+    _parameter_constraints: dict = {
+        **BaseADChecker._parameter_constraints,
+        "alpha": [Interval(RealNotInt, 0, 1, closed="neither")],
+    }
+
+    def __init__(
+        self,
+        alpha: float = 0.1,
+        n_jobs: Optional[int] = None,
+        verbose: Union[int, dict] = 0,
+    ):
+        super().__init__(
+            n_jobs=n_jobs,
+            verbose=verbose,
+        )
+        self.alpha = alpha
+
+    def fit(  # noqa: D102
+        self,
+        X: np.ndarray,
+        y: Optional[np.ndarray] = None,  # noqa: ARG002
+    ):
+        X = validate_data(self, X=X)
+
+        n, p = X.shape
+        self.X_mean_ = np.mean(X, axis=0)
+
+        # get inverse of covariance matrix for Mahalanobis distance
+        cov = np.cov(X, rowvar=False)  # rowvar=False: rows are samples
+        self.inv_cov_ = np.linalg.pinv(cov)  # pseudoinverse for numerical stability
+
+        # threshold based on critical value from the F-distribution
+        f_val = f_dist.ppf(self.alpha, p, n - p)
+        self.threshold_ = p * (n - 1) * (n + 1) / (n * (n - p)) * f_val
+
+    def predict(self, X: Union[np.ndarray, csr_array]) -> np.ndarray:  # noqa: D102
+        t2_values = self.score_samples(X)
+        passed = t2_values <= self.threshold_
+        return passed
+
+    def score_samples(self, X: np.ndarray) -> np.ndarray:
+        """
+        Calculate the applicability domain score of samples, i.e. the
+        T^2 statistic value for samples. Note that here lower score indicates
+        sample more firmly inside AD.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            The data matrix.
+
+        Returns
+        -------
+        scores : ndarray of shape (n_samples,)
+            Applicability domain scores of samples.
+        """
+        check_is_fitted(self)
+
+        X_centered = X - self.X_mean_
+        t2_values = np.array([(x @ self.inv_cov_ @ x.T) for x in X_centered])
+
+        return t2_values
