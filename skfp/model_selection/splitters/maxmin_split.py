026a41a15c7df2844c2315da4f4d9fa187cb07ee
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2025-06-03 12:46:54 +0200

    Add stratified MaxMin split (#456)

--- skfp/model_selection/splitters/maxmin_split.py
+++ skfp/model_selection/splitters/maxmin_split.py
@@ -2,6 +2,8 @@ from collections.abc import Sequence
 from numbers import Integral
 from typing import Any
 
+import numpy as np
+import pandas as pd
 from rdkit.Chem import Mol
 from rdkit.Chem.rdFingerprintGenerator import GetMorganGenerator
 from rdkit.SimDivFilters.rdSimDivPickers import MaxMinPicker
@@ -311,3 +313,318 @@ def maxmin_train_valid_test_split(
         return train_subset, valid_subset, test_subset, *additional_data_split
     else:
         return train_subset, valid_subset, test_subset
+
+
+@validate_params(
+    {
+        "data": ["array-like"],
+        "labels": ["array-like"],
+        "additional_data": ["tuple"],
+        "train_size": [
+            Interval(RealNotInt, 0, 1, closed="neither"),
+            Interval(Integral, 1, None, closed="left"),
+            None,
+        ],
+        "test_size": [
+            Interval(RealNotInt, 0, 1, closed="neither"),
+            Interval(Integral, 1, None, closed="left"),
+            None,
+        ],
+        "return_indices": ["boolean"],
+    },
+    prefer_skip_nested_validation=True,
+)
+def maxmin_stratified_train_test_split(
+    data: Sequence[str | Mol],
+    labels: np.ndarray | list[int] | pd.Series,
+    *additional_data: Sequence,
+    train_size: float | None = None,
+    test_size: float | None = None,
+    return_indices: bool = False,
+    random_state: int = 0,
+) -> (
+    tuple[Sequence[str | Mol], Sequence[str | Mol], Sequence[Sequence[Any]]]
+    | tuple[Sequence, ...]
+    | tuple[Sequence[int], Sequence[int]]
+):
+    """
+    Split using MaxMin algorithm with stratification.
+
+    A variant of MaxMin split (see :py:func:`maxmin_train_test_split`), modified to
+    split each class separately. The goal is to preserve the class distribution in
+    the resulting train and test splits, while also distributing points in each subset
+    across the chemical space.
+
+    Note that results may differ quite strongly from regular MaxMin split, as here
+    classes are treated independently of each other. While the distances between
+    compounds in each class are maximized, there are no guarantees for the overall
+    dataset. However, generally this split should also result in relatively uniform
+    coverage of the entire chemical space.
+
+    Resulting sizes of train and test sets may differ slightly for very small datasets.
+    This is because each class is split separately with a given percentage.
+
+    If ``train_size`` and ``test_size`` are integers, they must sum up to the ``data``
+    length. If they are floating numbers, they must sum up to 1.
+
+    Parameters
+    ----------
+    data : sequence
+        A sequence representing either SMILES strings or RDKit ``Mol`` objects.
+
+    labels : array-like
+        An array or list with class labels as integers.
+
+    additional_data: list[sequence]
+        Additional sequences to be split alongside the main data (e.g., labels or feature vectors).
+
+    train_size : float, default=None
+        The fraction of data to be used for the train subset. If None, it is set to 1 - test_size.
+        If test_size is also None, it will be set to 0.8.
+
+    test_size : float, default=None
+        The fraction of data to be used for the test subset. If None, it is set to 1 - train_size.
+        If train_size is also None, it will be set to 0.2.
+
+    return_indices : bool, default=False
+        Whether the method should return the input object subsets, i.e. SMILES strings
+        or RDKit ``Mol`` objects, or only the indices of the subsets instead of the data.
+
+    random_state: int, default=0
+        Random generator seed that will be used for selecting initial molecules.
+
+    Returns
+    -------
+    subsets : tuple[list, list, ...]
+        Tuple with train-test subsets of provided arrays. First two are lists of SMILES
+        strings or RDKit ``Mol`` objects, depending on the input type. Third and fourth
+        are NumPy arrays with labels of train and test subsets. If `return_indices` is
+        True, lists of indices are returned instead of actual data as the first two
+        elements.
+
+    See Also
+    --------
+    :func:`maxmin_train_test_split` : Regular MaxMin split.
+    """
+    data_arr = np.array(data)
+    labels = np.array(labels, dtype=int)
+
+    train_idxs = []
+    test_idxs = []
+
+    for label in np.unique(labels):
+        label_idxs = np.nonzero(labels == label)[0]
+        label_data = data_arr[label_idxs]
+
+        # split indices of current label into train and test
+        # then map them to indices of the entire dataset
+        label_train_idxs, label_test_idxs = maxmin_train_test_split(
+            label_data,
+            train_size=train_size,
+            test_size=test_size,
+            return_indices=True,
+            random_state=random_state,
+        )
+        label_train_idxs = label_idxs[label_train_idxs].tolist()
+        label_test_idxs = label_idxs[label_test_idxs].tolist()
+
+        train_idxs.extend(label_train_idxs)
+        test_idxs.extend(label_test_idxs)
+
+    if return_indices:
+        train_subset = train_idxs
+        test_subset = test_idxs
+    else:
+        train_subset = get_data_from_indices(data, train_idxs)
+        test_subset = get_data_from_indices(data, test_idxs)
+
+    labels_train = labels[train_idxs]
+    labels_test = labels[test_idxs]
+
+    if additional_data:
+        additional_data_split: list[Sequence[Any]] = split_additional_data(
+            list(additional_data), train_idxs, test_idxs
+        )
+        return (
+            train_subset,
+            test_subset,
+            labels_train,
+            labels_test,
+            *additional_data_split,
+        )
+    else:
+        return train_subset, test_subset, labels_train, labels_test
+
+
+@validate_params(
+    {
+        "data": ["array-like"],
+        "labels": ["array-like"],
+        "additional_data": ["tuple"],
+        "train_size": [
+            Interval(RealNotInt, 0, 1, closed="neither"),
+            Interval(Integral, 1, None, closed="left"),
+            None,
+        ],
+        "valid_size": [
+            Interval(RealNotInt, 0, 1, closed="neither"),
+            Interval(Integral, 1, None, closed="left"),
+            None,
+        ],
+        "test_size": [
+            Interval(RealNotInt, 0, 1, closed="neither"),
+            Interval(Integral, 1, None, closed="left"),
+            None,
+        ],
+        "return_indices": ["boolean"],
+    },
+    prefer_skip_nested_validation=True,
+)
+def maxmin_stratified_train_valid_test_split(
+    data: Sequence[str | Mol],
+    labels: np.ndarray | list[int] | pd.Series,
+    *additional_data: Sequence,
+    train_size: float | None = None,
+    valid_size: float | None = None,
+    test_size: float | None = None,
+    return_indices: bool = False,
+    random_state: int = 0,
+) -> (
+    tuple[Sequence[str | Mol], Sequence[str | Mol], Sequence[Sequence[Any]]]
+    | tuple[Sequence, ...]
+    | tuple[Sequence[int], Sequence[int]]
+):
+    """
+    Split using MaxMin algorithm with stratification.
+
+    A variant of MaxMin split (see :py:func:`maxmin_train_valid_test_split`), modified
+    to split each class separately. The goal is to preserve the class distribution in
+    the resulting train, valid, and test splits, while also distributing points in each
+    subset across the chemical space.
+
+    Note that results may differ quite strongly from regular MaxMin split, as here
+    classes are treated independently of each other. While the distances between
+    compounds in each class are maximized, there are no guarantees for the overall
+    dataset. However, generally this split should also result in relatively uniform
+    coverage of the entire chemical space.
+
+    Resulting sizes of train, valid, and test sets may differ slightly for very small
+    datasets. This is because each class is split separately with a given percentage.
+
+    If ``train_size``, ``valid_size`` and ``test_size`` are integers, they must sum up
+    to the ``data`` length. If they are floating numbers, they must sum up to 1.
+
+    Parameters
+    ----------
+    data : sequence
+        A sequence representing either SMILES strings or RDKit ``Mol`` objects.
+
+    labels : array-like
+        An array or list with class labels as integers.
+
+    additional_data: sequence
+        Additional sequences to be split alongside the main data, e.g. labels.
+
+    train_size : float, default=None
+        The fraction of data to be used for the train subset. If None, it is set
+        to 1 - test_size - valid_size. If valid_size is not provided, train_size
+        is set to 1 - test_size. If train_size, test_size and valid_size aren't
+        set, train_size is set to 0.8.
+
+    valid_size : float, default=None
+        The fraction of data to be used for the test subset. If None, it is set
+        to 1 - train_size - valid_size. If train_size, test_size and valid_size
+        aren't set, train_size is set to 0.1.
+
+    test_size : float, default=None
+        The fraction of data to be used for the validation subset. If None, it is
+        set to 1 - train_size - valid_size. If valid_size is not provided, test_size
+        is set to 1 - train_size. If train_size, test_size and valid_size aren't set,
+        test_size is set to 0.1.
+
+    return_indices : bool, default=False
+        Whether the method should return the input object subsets, i.e. SMILES strings
+        or RDKit ``Mol`` objects, or only the indices of the subsets instead of the data.
+
+    random_state: int, default=0
+        Random generator seed that will be used for selecting initial molecules.
+
+    Returns
+    -------
+    subsets : tuple[list, list, ...]
+        Tuple with train-valid-test subsets of provided arrays. First three are lists of
+        SMILES strings or RDKit ``Mol`` objects, depending on the input type. Next three
+        are NumPy arrays with labels of train-valid-test subsets. If `return_indices` is
+        True, lists of indices are returned instead of actual data as the first three
+        elements.
+
+    See Also
+    --------
+    :func:`maxmin_train_valid_test_split` : Regular MaxMin split.
+    """
+    data_arr = np.array(data)
+    labels = np.array(labels, dtype=int)
+
+    train_idxs = []
+    valid_idxs = []
+    test_idxs = []
+
+    for label in np.unique(labels):
+        label_idxs = np.nonzero(labels == label)[0]
+        label_data = data_arr[label_idxs]
+
+        # split indices of current label into train and test
+        # then map them to indices of the entire dataset
+        label_train_idxs, label_valid_idxs, label_test_idxs = (
+            maxmin_train_valid_test_split(
+                label_data,
+                train_size=train_size,
+                valid_size=valid_size,
+                test_size=test_size,
+                return_indices=True,
+                random_state=random_state,
+            )
+        )
+        label_train_idxs = label_idxs[label_train_idxs].tolist()
+        label_valid_idxs = label_idxs[label_valid_idxs].tolist()
+        label_test_idxs = label_idxs[label_test_idxs].tolist()
+
+        train_idxs.extend(label_train_idxs)
+        valid_idxs.extend(label_valid_idxs)
+        test_idxs.extend(label_test_idxs)
+
+    if return_indices:
+        train_subset = train_idxs
+        valid_subset = valid_idxs
+        test_subset = test_idxs
+    else:
+        train_subset = get_data_from_indices(data, train_idxs)
+        valid_subset = get_data_from_indices(data, valid_idxs)
+        test_subset = get_data_from_indices(data, test_idxs)
+
+    labels_train = labels[train_idxs]
+    labels_valid = labels[valid_idxs]
+    labels_test = labels[test_idxs]
+
+    if additional_data:
+        additional_data_split: list[Sequence[Any]] = split_additional_data(
+            list(additional_data), train_idxs, valid_idxs, test_idxs
+        )
+        return (
+            train_subset,
+            valid_subset,
+            test_subset,
+            labels_train,
+            labels_valid,
+            labels_test,
+            *additional_data_split,
+        )
+    else:
+        return (
+            train_subset,
+            valid_subset,
+            test_subset,
+            labels_train,
+            labels_valid,
+            labels_test,
+        )
