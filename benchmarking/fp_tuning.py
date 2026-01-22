edf6297f24f7131b1d8156032e9fb9384b76bdf2
AStaniszewski <staniszewski01adam@gmail.com>
2026-01-22 09:32:57 +0100

    Combine train and valid into train inside fp tuning benchmark

--- benchmarking/fp_tuning.py
+++ benchmarking/fp_tuning.py
@@ -156,7 +156,10 @@ if __name__ == "__main__":
         print("DATASET", dataset_name)
         X = np.array(X)
 
-        train_idxs, valid_idxs, test_idxs = load_ogb_splits(dataset_name)
+        split_idx = load_ogb_splits(dataset_name, as_dict=True)
+
+        train_idxs = list(split_idx["train"]) + list(split_idx["valid"])
+        test_idxs = split_idx["test"]
 
         smiles_train = X[train_idxs]
         smiles_test = X[test_idxs]
